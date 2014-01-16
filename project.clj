d3dfef66919f1ae725c7a8b4c4c02c3631cf0722
Ryan Berdeen <rberdeen@hubspot.com>
2014-01-16 13:01:43 -0500

    url etc

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject com.ryanberdeen/lead-graphite-server "0.1.0-SNAPSHOT"
+  :description "Act as a Graphite cluster server"
+  :url "https://github.com/also/lead-graphite-server"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.ryanberdeen/lead "0.1.0-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]])
